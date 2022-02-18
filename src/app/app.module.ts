74ce1f52e5d13f84a33c6159e8ed3d4941304bbd
iskonplus <58971909+iskonplus@users.noreply.github.com>
2022-02-18 20:22:28 +0300

    added basic app markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components//header/header.component';
+import { FooterComponent } from './components//footer/footer.component';
+import { MainComponent } from './components//main/main.component';
 
 @NgModule({
   declarations: [
