c9c35703911d9a84b00fd90c75bf906bda2d6f2d
iskonplus <58971909+iskonplus@users.noreply.github.com>
2022-02-22 14:29:05 +0300

    delete console log

--- src/app/services/user-page.service.ts
+++ src/app/services/user-page.service.ts
@@ -21,25 +21,12 @@ export class UserPageService {
     usersdateLocalStorage.forEach((el: any) => {
       if ((el.email === data.email && el.password === data.password)) {
         el = {...data};
-        console.log(el)
-        console.log(usersdateLocalStorage)
+        
         localStorage.setItem('users', JSON.stringify(usersdateLocalStorage));
         localStorage.setItem('activeUser', JSON.stringify(data));
       }
     });
 
-    // if (!localStorage.getItem('users')) {
-    //   localStorage.setItem('users', JSON.stringify([data]));
-    // } else {
-    //   const usersLocalStorage = JSON.parse(localStorage.getItem('users') || '');
-    //   usersLocalStorage.push(data);
-    //   localStorage.setItem('users', JSON.stringify(usersLocalStorage));
-    // }
-
   }
-
-
-
-
 
 }
